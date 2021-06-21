f7b94db8fa4bf0ada90b02e096a09a9b0dbbc368
marciobahia <marciosellabahia@hotmail.com>
2021-06-21 16:25:10 -0300

    Add server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express, { response } from "express";
 
 
 // @types/express
 const app = express();
 
+app.get("/test", (request, response) => {
+   return response.send("Olá Mundo");
+});
+
+app.post("/test-post", (Request, response) => {
+    return response.send("Olá valoriza método POST");
+});
+
 // http://localhost:3000
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, () => console.log("Server is running Valoriza"));
 
 
